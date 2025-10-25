640c0f2c40882ae714fabdde6d8504d31ea2d2de
New_Developer <137284770+XBotHanter@users.noreply.github.com>
2025-10-25 10:23:09 +0300

    #(v1.3.1)
    #(v1.3.1) - В функции деления мы исправили баг с делением на 0

--- Калькулятор.py
+++ Калькулятор.py
@@ -8,13 +8,15 @@ def Multiply(a,b):
     return a*b
 
 def Div(a,b):
-    return a/b
+    if(b==0): return "На ноль делить нельзя"
+    else: return a/b
 
 print(sum(10,20))#выведет 30
 print(sub(20,10))#выведет 10
 print(Multiply(10,5))#выведет 50
 print(Div(50,10))#выведет 5
-#(v1.3.0) - Добавляем фукнцию деления
+#(v1.3.1) - В функции деления мы исправили баг с делением на 0
+
 
 
 

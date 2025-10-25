bf38e0779e03d73feaaed900d51043c469c902f5
New_Developer <137284770+XBotHanter@users.noreply.github.com>
2025-10-25 10:37:27 +0300

    #(v2.0.1)
    #(v2.0.1) - Исправлен баг с выбором операций

--- Калькулятор.py
+++ Калькулятор.py
@@ -15,11 +15,13 @@ def calc(a,b,str):
     if(str=="Сложить"): return sum(a,b)
     elif(str=="Вычесть(отнять)"): return sub(a,b)
     elif(str=="Умножить"):return Multiply(a,b)
-    else: return Div(a,b)
+    elif(str=="Деление"): return Div(a,b)
+    else:return "Такой операции нет"
 
 
 print(calc(30,12,"Сложить"))
-#(v2.0.0) - Полностью обновленное приложение,в котором мы можем выбирать операцию над числами
+#(v2.0.1) - Исправлен баг с выбором операций
+
 
 
 
